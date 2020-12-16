1971bfd67ef7526926ecf2d4d918fc3e0496b8c3
Aleksandar Stojanovic <aleksandar_stojanovic@station24-doboj.netex.ro>
2020-12-16 13:46:09 +0100

    finished spring boot part

--- src/main/java/com/aleks/administrator/employees/controllers/ApplicationController.java
+++ src/main/java/com/aleks/administrator/employees/controllers/ApplicationController.java
@@ -1,11 +1,18 @@
 package com.aleks.administrator.employees.controllers;
 
+import com.aleks.administrator.employees.models.Employee;
 import com.aleks.administrator.employees.services.EmployeeService;
+import com.aleks.administrator.workers.models.Worker;
 import com.aleks.administrator.workers.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 public class ApplicationController {
@@ -22,4 +29,12 @@ public class ApplicationController {
         return "index";
 
     }
+
+    @RequestMapping(value="/both/addNew", method = {RequestMethod.POST, RequestMethod.PUT, RequestMethod.GET})
+    public String addEmployee(Employee employee, Worker worker, Model model) {
+        workerService.addWorker(worker);
+        employeeService.addEmployee(employee);
+        return "redirect:/index";
+    }
+
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,15 +14,32 @@ $(document).ready(function() {
        $('#editModal').modal();
    });
 
-   $('.table .deleteButton').on('click', function(event){
+    $('.table .editButton1').on('click', function (event){
+        event.preventDefault();
+
+        var href = $(this).attr('href');
+        $.get(href, function(employee, status){
+            $('#editId1').val(employee.id);
+            $('#editName1').val(employee.name);
+            $('#editPosition1').val(employee.position);
+            $('#editDepartment1').val(employee.department);
+            $('#editDate1').val(employee.updateOn.substr(0,10));
+        });
+        $('#editModal1').modal();
+    });
+
+    $('.table .deleteButton').on('click', function(event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $('#deleteModal #deleteId').attr('href', href)
+        $('#deleteModal').modal();
+    });
+
+   $('.table .deleteButton1').on('click', function(event){
        event.preventDefault();
        var href = $(this).attr('href');
-       $('#deleteModal #deleteId').attr('href', href)
-       $('#deleteModal').modal();
-   });
-
-   $('.addButton .selectDb').on('click', function(event){
-       event.preventDefault();
+       $('#deleteModal1 #deleteId1').attr('href', href)
+       $('#deleteModal1').modal();
    });
 
 });
